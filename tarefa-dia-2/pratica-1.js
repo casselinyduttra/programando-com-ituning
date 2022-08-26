cd0dbb2a38965a3a68bea62c8a91f5add198e159
Casseliny Dutra <casseliny.l@gmail.com>
2022-08-27 00:39:57 +0100

    Acrescentando mais linhas de codigo

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,5 +1,17 @@
-let nome ="Casseliny Dutra"
+let nome = "Casseliny Dutra"
 let email = "casseliny.l@gmail.com"
 let profissao = "estudante"
 let idade = 28
 let viagemDosSonhos = "Maldivas"
+
+console.log(nome)
+console.log(email)
+console.log(idade)
+console.log(profissao)
+console.log(viagemDosSonhos)
+
+console.log("Ola " + nome + " Somos aqui da agencia de turismo Viagem dos Sonhos.")
+console.log("Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para " + viagemDosSonhos)
+console.log("Confirma pra gente alguns dados? A sua idade e " + idade)
+console.log("voce e" + profissao + " e o seu email e: " + email)
+console.log("Ficamos no aguardo. Muito obrigado e boa viagem! :D")
