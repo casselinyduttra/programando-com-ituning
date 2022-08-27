881190ae9fd80a7f53ceffc1ecddaf8b97384b5d
Casseliny Dutra <casseliny.l@gmail.com>
2022-08-27 03:55:43 +0100

    Atualizando ações

---
+++ tarefa-dia-2/pratica-4.js
@@ -0,0 +1,10 @@
+let produtoA = {"nome": "camiseta", "valor": 100.00, "internacional": true};
+let produtoB = {"nome": "perfume", "valor": 200.00, "internacional": true};
+let produtoC = {"nome": "sandália", "valor": 120.00, "internacional": false};
+
+if (produtoA = true){
+    console.log("Produto Internacional")
+}
+else {
+    console.log("Produto Nacional")
+}
